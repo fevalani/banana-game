aaefcddeef28aae5e817ef37cdb07b654e55c24d
Fernando Valani Cavalcante <valanife@gmail.com>
2021-08-27 16:04:35 -0300

    create canvas

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const screenWidth = window.innerWidth;
+const screenHeitgh = window.innerHeight;
+const canvas = document.querySelector(".canvas") as HTMLCanvasElement;
+
+const score = document.querySelector(".score") as HTMLCanvasElement;
+
+score.innerText = "Score muda aqui";
